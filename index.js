ff2409fbb437f0d9a70039ba181847797e83bac5
frankod19 <fd.gomez1024@gmail.com>
2024-09-11 15:12:15 -0500

    docx-templater

--- index.js
+++ index.js
@@ -7,36 +7,43 @@ const template = fs.readFileSync(process.argv[2]);
 
 createReport({
   template,
+  cmdDelimiter:['{{','}}'],
   data: {
     allFilms: {
-      films:[]
-      // films: [
-      //   {
-      //     title: "A New Hope",
-      //     releaseDate: "1977-05-25",
-      //   },
-      //   {
-      //     title: "The Empire Strikes Back",
-      //     releaseDate: "1980-05-17",
-      //   },
-      //   {
-      //     title: "Return of the Jedi",
-      //     releaseDate: "1983-05-25",
-      //   },
-      //   {
-      //     title: "The Phantom Menace",
-      //     releaseDate: "1999-05-19",
-      //   },
-      //   {
-      //     title: "Attack of the Clones",
-      //     releaseDate: "2002-05-16",
-      //   },
-      //   {
-      //     title: "Revenge of the Sith",
-      //     releaseDate: "2005-05-19",
-      //   },
-      // ],
+      // films:[]
+      films: [
+        {
+          title: "A New Hope",
+          releaseDate: "1977-05-25",
+        },
+        {
+          title: "The Empire Strikes Back",
+          releaseDate: "1980-05-17",
+        },
+        {
+          title: "Return of the Jedi",
+          releaseDate: "1983-05-25",
+        },
+        {
+          title: "The Phantom Menace",
+          releaseDate: "1999-05-19",
+        },
+        {
+          title: "Attack of the Clones",
+          releaseDate: "2002-05-16",
+        },
+        {
+          title: "Revenge of the Sith",
+          releaseDate: "2005-05-19",
+        },
+      ],
     },
+    test:{name:'kimito',ad:'purra'},
+    bebes:[
+      {name:'Luisa'},
+      {name:'Maria'},
+      {name:'Camila'},
+    ]
   },
   additionalJsContext: {
     tile: async (z, x, y, size = 3) => {
